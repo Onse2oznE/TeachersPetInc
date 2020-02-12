70d0f9a5bfbefa42280c9462565af8ec63f644b5
OzonE <31847787+ozne05onse@users.noreply.github.com>
2020-02-12 19:57:18 +0800

    update

--- server/public/database/conn.js
+++ server/public/database/conn.js
@@ -1,7 +1,22 @@
+var mysql = require('mysql');
+
+//CONNECT TO DATABASE
 // const con = mysql.createConnection({
 //   host: '',
 //   user: '',
 //   password: '',
 //   database: ''
 // });
+
+
+//CHECK CONNECTION TO DATABASE
+
+// con.connect(function(err){
+// if(!err) {
+//     console.log("Database is connected");
+// } else {
+//     console.log("Error while connecting with database");
+// }
+// });
+// module.exports = con; 
 
